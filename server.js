8215a53c8caf38332891c2516463caf7b738d556
Ram Chandra Nayak <ramnayak778800@gmail.com>
2026-04-13 14:31:43 +0530

    Fix: use absolute path for static files serving

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const os = require('os');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 // Detect the compiled binary based on OS
